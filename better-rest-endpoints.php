8889ea50c62dc9be09083a284435a4c55f0cff4d
admin <qamardevelopers@gmail.com>
2020-03-17 16:49:10 +0500

    Frist Test Change

--- better-rest-endpoints.php
+++ better-rest-endpoints.php
@@ -11,7 +11,7 @@ License URI:  https://www.gnu.org/licenses/gpl-3.0.html
 Text Domain:  better-rest-endpoints
 Domain Path:  /languages
 
-Better Rest Endpoints is free software: you can redistribute it and/or modify
+Better Rest Endpoint is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 any later version.
